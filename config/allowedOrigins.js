e5e5f84c2264672ce505dbf38d04b337c5acee56
Sagar Sunny <sagarsunny2403@gmail.com>
2026-06-02 14:07:29 +0530

    added allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,5 +3,5 @@ export const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
   'https://server-task-manager-xhbh.onrender.com',
-  'https://fullstack-task-manager-lyart.vercel.app/'
+  'https://fullstack-task-manager-lyart.vercel.app'
 ];
