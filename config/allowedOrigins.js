00ea1e34bb6d4cd4376b143d495f926a30c874e5
Sagar Sunny <sagarsunny2403@gmail.com>
2026-06-02 14:03:29 +0530

    added allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 export const allowedOrigins = [
   'https:www.mysite.com',
   'http://localhost:5173',
-  'http://localhost:3000'
+  'http://localhost:3000',
+  'https://server-task-manager-xhbh.onrender.com'
 ];
